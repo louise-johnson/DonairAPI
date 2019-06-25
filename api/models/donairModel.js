c0d91a290cfb41b5257ca608bd3161dcb4f39c19
Louise Johnson <49207144+louise-johnson@users.noreply.github.com>
2019-06-25 11:56:24 -0300

    Added price property
    The price is calculated before saving a record in the db. Price = donairPrice * (1-discountPercent/100)

--- api/models/donairModel.js
+++ api/models/donairModel.js
@@ -2,6 +2,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//Donair prices
+var largeDonairPrice = 20;
+var medDonairPrice = 15;
+var smallDonairPrice = 10;
+
 var DonairSchema = new Schema({
 	name: {
 		type: String,
@@ -28,7 +33,18 @@ var DonairSchema = new Schema({
 		min: [0, "Minimum discount percentage is 0."],
 		max: [100, "Maximum discount percentage is 100."],
 		default: 0
+	},
+	price: {
+		type: Number
 	}
+});
+
+// Calculate the price of the Donair and populate the price field of the schema
+DonairSchema.pre('save', function (next) {
+	if(this.size == 'large') this.price = (1-this.discountPercent/100)*largeDonairPrice;
+	if(this.size == 'medium') this.price = (1-this.discountPercent/100)*medDonairPrice;
+	if(this.size == 'small') this.price = (1-this.discountPercent/100)*smallDonairPrice;
+  next();
 });
 
 module.exports = mongoose.model('Donairs', DonairSchema);
